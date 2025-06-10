aa3c51d5b9de9e3939f5b255ad60d87192a850c8
Phong Hoang <hoangvanphong102@gmail.com>
2025-06-10 16:19:34 +0700

    Init Spring project

---
+++ backend/movtube/src/main/java/fpt/hsf302/movtube/MovtubeApplication.java
@@ -0,0 +1,13 @@
+package fpt.hsf302.movtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovtubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovtubeApplication.class, args);
+    }
+
+}
---
+++ backend/movtube/src/test/java/fpt/hsf302/movtube/MovtubeApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.hsf302.movtube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovtubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
